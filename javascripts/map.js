84089745fc74a60919e9edbd1da896f04ca759d7
LinaBonill <lmbonillr@correo.udistrital.edu.co>
2022-11-26 20:53:00 -0500

    Inicio de nuevo

--- javascripts/map.js
+++
@@ -1,9 +0,0 @@
-var map = L.map('main_map').setView([-34.6012424,-58.3861497],13);
-
-L.titleLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-L.maker([-34.6012424,-58.3861497]).addTo(map);
-L.maker([-34.596932,-58.3808287]).addTo(map);
-L.maker([-34.599564,-58.3778777]).addTo(map);
