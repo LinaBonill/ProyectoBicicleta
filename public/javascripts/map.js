cd4eb1ca038718f7c17a2cf0be0e285b9763787c
LinaBonill <lmbonillr@correo.udistrital.edu.co>
2022-11-27 18:14:27 -0500

    Integracion de API, pero falla al poner bicicletas en el mapa

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,9 +1,18 @@
 var map = L.map('main_map').setView([-34.6012424, -58.3861497], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-L.marker([-34.6012424,-58.3861497]).addTo(map);
-L.marker([-34.596932,-58.3808287]).addTo(map);
-L.marker([-34.599564,-58.3778777]).addTo(map);
+
+
+$.ajax({
+    dataType:"json",
+    url:"api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.foreach(function(bici){
+            L.marker(bici.ubicacion, {title:bici.id} ).addTo(map);  
+        });
+    }
+    
+});
